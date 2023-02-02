533ff96f1c04f4d964b9ff01e147222053237ae7
Niclas Schad <niclas.schad@stackit.de>
2023-02-02 11:07:19 +0100

    add api defaulting for enforceNodeAllocatable
    Signed-off-by: Niclas Schad <niclas.schad@stackit.de>

--- pkg/apis/core/v1beta1/defaults.go
+++ pkg/apis/core/v1beta1/defaults.go
@@ -274,6 +274,10 @@ func SetDefaults_Shoot(obj *Shoot) {
 			}
 		}
 
+		if len(obj.Spec.Kubernetes.Kubelet.EnforceNodeAllocatable) == 0 {
+			obj.Spec.Kubernetes.Kubelet.EnforceNodeAllocatable = []string{"pods"}
+		}
+
 		if obj.Spec.Maintenance.AutoUpdate.MachineImageVersion == nil {
 			obj.Spec.Maintenance.AutoUpdate.MachineImageVersion = pointer.Bool(true)
 		}
